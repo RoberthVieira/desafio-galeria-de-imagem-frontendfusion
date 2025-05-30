d67ccf9fbfb035aeb99389b468630e36f189d4a7
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-05-29 23:56:03 -0300

    feat: Implementa carrossel de imagens com funções avançarCarrosel e voltarCarrosel para navegação

--- src/components/GaleriaImg/GaleriaImg.jsx
+++ src/components/GaleriaImg/GaleriaImg.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react";
 import fetchPicsum from '../../services/fetchPicsum';
 
+import ArrowBtn from "../Button/ArrowBtn";
+
 import styles from './GaleriaImg.module.css';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-export default function GaleriaImg(){
+export default function GaleriaImg() {
 
+    const [offset, setOffset] = useState(0);
     const [imagens, setImagens] = useState([]);
+
+    const larguraImagem = 310; 
+    const imgsVisiveis = 3;  
 
     useEffect(() => {
         fetchPicsum()
@@ -13,7 +20,20 @@ export default function GaleriaImg(){
             .catch(error => {
                 console.log('Error:', error)
             })
-    }, [])
+    }, []);
+
+    function avançarCarrosel() {
+        const totalOffset = (imagens.length - imgsVisiveis) * larguraImagem;
+        if(offset < totalOffset){
+            setOffset(offset + larguraImagem);
+        }
+    }
+
+    function voltarCarrosel() {
+        if (offset > 0) {
+            setOffset(offset - larguraImagem)
+        }
+    }
 
     return (
         <main className={styles.galeryContainer}>
@@ -23,12 +43,24 @@ export default function GaleriaImg(){
                     Explore uma seleção de imagens incríveis capturadas por fotógrafos ao redor do mundo.
                 </p>
             </div>
-            <div className={styles.galeryImgContainer}>
-                {imagens.map((img) => (
-                    <figure key={img.download_url}>
-                        <img src={img.download_url} alt={img.author} className={styles.imgGalery}/>
-                    </figure>
-                ))}
+            <div className={styles.containerCarrosel}>
+                <ArrowBtn btnContent={<FiChevronLeft color="#d9cbb3" />} eventClick={voltarCarrosel} />
+                <div className={styles.galeryImgContainer}>
+                    <div
+                        className={styles.deslizeImg}
+                        style={{ transform: `translateX(-${offset}px)`, transition: 'transform 0.5s ease' }}
+                    >
+                        {imagens.map((img) => (
+                            <figure key={img.download_url}>
+                                <img 
+                                    src={img.download_url} 
+                                    alt={img.author} 
+                                    className={styles.imgGalery} />
+                            </figure>
+                        ))}
+                    </div>
+                </div>
+                <ArrowBtn btnContent={<FiChevronRight color="#d9cbb3" />} eventClick={avançarCarrosel} />
             </div>
         </main>
     )
