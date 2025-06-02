438ed1bb39b6788500dafd5fee33d955e80ac22b
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-06-01 22:01:27 -0300

    style: Adicionado animação no componente GaleriaImg e logo ao projeto.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import styles from './Header.module.css';
+import Logo from '../../../public/galeria.png'
 export default function Header() {
     return (
         <header className={styles.headerContainer}>
             <h1>Galeria de Imagens</h1>
+            <img src={Logo} alt="Logo" width={40}/>
         </header>
     )
 }
